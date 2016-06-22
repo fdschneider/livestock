d564f71cc3d107760c5f4672fbd785b39843956e
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-06-22 16:09:26 +0200

    add meanfield version of livestock model

--- R/livestock.r
+++ R/livestock.r
@@ -27,11 +27,23 @@ d_rho_11 <- function(rho,  parms) {
   return(growth - mortality)
 }
 
+#' @export
 
+d_rho_mean <- function(rho, parms) {
+  growth <- with(parms, r *  rho[1]^( 1 + alpha) * (b + (1-b) * f * rho[1])  * (1 - rho[1]/(K * (1-c*rho[1]) ) ))
+  if(growth <= 0| is.na(growth)) {growth <- 0}
+
+  mortality <- with(parms,  m * rho[1] + ( (a+v*rho[1]) * (1-p*rho[1]) * rho[1]^( 1 + q) * L )/(1 + (a+v*rho[1]) * (1-p*rho[1]) * h * rho[1]^( 1 + q)) )
+  if(mortality <= 0 | is.na(mortality)) {mortality <- 0}
+
+  return(growth - mortality)
+
+}
 
 #' @export
 
 livestock <- list(
+  template = ini_rho(rho_1 = 0.9999),
   defparms = list(
     r = 1,
     b = 1,
@@ -48,21 +60,32 @@ livestock <- list(
     p = 0
   ),
 
-  odesys = function(t, rho, parms = model_parms, type = c("pair", "mean", "ca")) {
-  if(rho[1] < 1e-7 ) {
-    rho <- ini_rho(0,0)
+  pair = function(t, rho, parms = model_parms) {
+    if(rho[1] < 1e-6) {
+      rho <- ini_rho(0,0)
+      out <- list(c(
+        rho_1 = 0,
+        rho_11 = 0
+      )  )
+    } else {
+      rho_1 <- d_rho_1(rho, parms)
+      rho_11 <- d_rho_11(rho, parms)
+      out <- list(c(
+        rho_1 = rho_1,
+        rho_11 = rho_11
+      )  )
+    }
+    return(out)
+  },
+
+  meanfield = function(t, rho, parms = model_parms) {
+
+    delta <- d_rho_mean(rho, parms )
+
+    list(c(
+          rho_1 = delta,
+         rho_11 = delta
+         ))
   }
 
-  rho_1 <- d_rho_1(rho, parms)
-  rho_11 <- d_rho_11(rho, parms)
-
-  list(c(
-    rho_1 = rho_1,
-    rho_11 = rho_11#,
-    #changes of other pairs and singletons are not calculated
-    #rho_10 = 0,
-    #rho_00 = 0,
-    #rho_0 = 1-rho_1
-  )  )
-}
 )
