2abb2c77e13fe0cf506a2a16f0836e37294faa37
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-06-23 16:17:24 +0200

    update growth and mortality function for plotting

--- R/growth.r
+++ R/growth.r
@@ -8,21 +8,9 @@
 #' @return
 #' @export
 
-growth <- function(rho_1, q_01 = "auto", parms = livestock$defparms, set = list(NA)) {
+growth <- function(rho_1, q_01 = "auto", parms = livestock$defparms) {
 
-  if(q_01[1] == "auto") q_01 <- 1
-
-
-  parms.names <- names(parms)
-  set.names <- names(set)
-  m.names <- sort(unique(c(parms.names, set.names)))
-
-  parms_temp <- sapply(m.names, function(i) {
-    if (i %in% set.names) set[[i]]
-    else parms[[i]]
-  }, simplify = FALSE)
-
-  out <- with(parms_temp, r*(b + (1-b)*f*q_01)*rho_1^(1+alpha)*(1-rho_1/(K * (1-c * q_01) ) ))
+  out <- with(parms, r*(b + (1-b)*f*q_01)*rho_1^(1+alpha)*(1-rho_1/(K * (1-c * q_01) ) ))
 
   out[out < 0] <- 0
   return(out)
--- R/mortality.r
+++ R/mortality.r
@@ -10,5 +10,8 @@
 #'
 mortality <- function(rho_1, q_11 = 1, parms = livestock$defparms) {
 
-  with(parms, (m * rho_1) +( (a + v*q_11 )*(1-p*q_11) *rho_1^(1+q)*L)/(1+(a + v*q_11 )*(1-p*q_11)*h*(rho_1^(1+q)) ) )
+  out <- with(parms, (m * rho_1) +( (a + v*q_11 )*(1-p*q_11) *rho_1^(1+q)*L)/(1+(a + v*q_11 )*(1-p*q_11)*h*(rho_1^(1+q)) ) )
+
+  out[out < 0] <- 0
+  return(out)
 }
