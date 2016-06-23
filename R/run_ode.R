10d572c33a985e47c6ec1a5443603f5865a41aa1
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-06-23 16:18:35 +0200

    rename sim into run_ode()

--- R/run_ode.R
+++ R/run_ode.R
@@ -9,8 +9,13 @@
 #' @export
 #'
 #' @examples
+#' p <- set_parms(livestock$defparms, set = list(b = 0.2, f = 0.9, p = 0.8))
+#' out <- run_ode(ini_rho(0.0001), livestock$pair, parms = p)
+#' plot(out[,1:2], type = "l", ylim = c(0,1))
 
-sim <- function(input, model, times =  exp(seq(0,4,length = 100))-1, parms = parms) {
- out <-  ode(y = ini_rho(input$rho_1), func = model, times, parms)
- return(as.data.frame(out))
+run_ode <- function(y, func, times = 1.05^seq(0,100,1), parms, ...) {
+  out <- deSolve::ode(y = y, func = func, times = times, parms = parms, ...)
+  out[out < 1e-06] <- 0
+  return(round(out,6))
+  return(as.data.frame(out))
 }
