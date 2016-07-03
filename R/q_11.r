c9a6afd97995b3d6091431e9474a87df3d69c1cb
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-07-03 17:49:21 +0200

    q_11 and q_01: return unnamed vector

--- R/q_11.r
+++ R/q_11.r
@@ -7,11 +7,19 @@
 #' @return
 #' @export
 #'
+#'
+#' @examples
+#'
+#'
+#'
+#' rho <- ini_rho(0.5,0.45)
+#' q_11(rho[1], rho[2])
+#'
 q_11 <- function(rho_1, rho_11, zlim = c(0,1)) {
   out <- rho_11/rho_1
   out[out < zlim[1] | is.na(out)] <- 0
   out[out > zlim[2]] <-  zlim[2]
-  return(out)
+  return(as.vector(out))
 
 }
 
@@ -28,6 +36,6 @@ q_01 <-  function(rho_1, rho_11, zlim = c(0,1)) {
   out <- (rho_1-rho_11)/(1-rho_1)
   out[out < zlim[1] | is.na(out)] <- 0
   out[out > zlim[2]] <-  zlim[2]
-  return(out)
+  return(as.vector(out))
 
 }
