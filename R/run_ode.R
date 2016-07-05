b660d0747606048ac92dcee91c23ce8fae6fafdc
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-07-05 09:49:07 +0200

    run_ode: update examples

--- R/run_ode.R
+++ R/run_ode.R
@@ -9,9 +9,12 @@
 #' @export
 #'
 #' @examples
-#' p <- set_parms(livestock$defparms, set = list(b = 0.2, f = 0.9, p = 0.8))
-#' out <- run_ode(ini_rho(0.0001), livestock$pair, parms = p)
+#' p <- set_parms(livestock$parms, set = list(b = 0.2, f = 0.9, p = 0.8))
+#' out <- run_ode(ini_rho(0.5,0.45), livestock$pair, parms = p)
 #' plot(out[,1:2], type = "l", ylim = c(0,1))
+#'
+#' out <- run_ode(ini_rho(0.5), livestock$meanfield, parms = p)
+#' lines(out[,1:2])
 
 run_ode <- function(y, func, times = 1.05^seq(0,100,1), parms, ...) {
   out <- deSolve::ode(y = y, func = func, times = times, parms = parms, ...)
