149381918bcacad37427e1321db6c0bae0e53091
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-07-05 09:46:52 +0200

    update livestock.r: simplify pair approximation and meanfield approximation func

--- R/livestock.r
+++ R/livestock.r
@@ -1,46 +1,4 @@
 
-#' @export
-
-d_rho_1 <- function(rho, parms) {
-
-  growth <- with(parms,
-                 r * (b + (1 - b) * f * q_01(rho[1],rho[2]) ) * rho[1]^( 1 + alpha) * (1 - rho[1]/(K * (1-c*q_01(rho[1],rho[2])) ) ))
-  if(growth <= 0 | is.na(growth)) {growth <- 0}
-
-  mortality <- with(parms, m * rho[1] + ( (a + v*q_11(rho[1],rho[2])) * (1 - p * q_11(rho[1],rho[2])) * rho[1]^( 1 + q) * L)/(1 + (a + v*rho[2]/rho[1]) * (1 - p * q_11(rho[1],rho[2])) * h  * rho[1]^( 1 + q)) )
-  if(mortality <= 0 | is.na(mortality)) {mortality <- 0}
-
-  return(growth - mortality)
-}
-
-
-#' @export
-
-d_rho_11 <- function(rho,  parms) {
-
-  growth <- with(parms,
-                 2* (rho[1] - rho[2]) * r * (b + (1 - b) * f * q_01(rho[1],rho[2]) ) * rho[1]^( 1 + alpha) * (1 - rho[1]/(K * (1-c*q_01(rho[1],rho[2])) ) ) / (1-rho[1]))
-  if(growth <= 0 | is.na(growth)) growth <- 0
-
-  mortality <- with(parms, 2 * rho[2] * m  + 2 * rho[2] * ( (a + v*rho[2]/rho[1]) * (1 - p * rho[2]/rho[1])  * rho[1]^( 1 + q) * L )/(1 +(a + v*rho[2]/rho[1]) * (1 - p * rho[2]/rho[1])  * h  * rho[1]^( 1 + q))  )
-  if(mortality <= 0 | is.na(mortality)) mortality <- 0
-
-  return(growth - mortality)
-}
-
-#' @export
-
-d_rho_mean <- function(rho, parms) {
-  growth <- with(parms, r *  rho[1]^( 1 + alpha) * (b + (1-b) * f * rho[1])  * (1 - rho[1]/(K * (1-c*rho[1]) ) ))
-  if(growth <= 0| is.na(growth)) {growth <- 0}
-
-  mortality <- with(parms,  m * rho[1] + ( (a+v*rho[1]) * (1-p*rho[1]) * rho[1]^( 1 + q) * L )/(1 + (a+v*rho[1]) * (1-p*rho[1]) * h * rho[1]^( 1 + q)) )
-  if(mortality <= 0 | is.na(mortality)) {mortality <- 0}
-
-  return(growth - mortality)
-
-}
-
 #' @title Livestock resilience model
 #'
 #' @description A model of resilience in arid rangelands.
@@ -95,29 +53,32 @@ livestock <- list(
     p = 0
   ),
   pair = function(t, rho, parms = model_parms) {
+
+    # insert equations here
+    delta_1 = (1-rho[1]) * colonization(rho, parms) - rho[1] * death(rho, parms)
+    delta_11 = 2 * (rho[1]-rho[2]) * colonization(rho, parms) - 2 * rho[2] * death(rho, parms)
+
     if(rho[1] < 1e-6) {
-      rho <- ini_rho(0,0)
       out <- list(c(
         rho_1 = 0,
         rho_11 = 0
       )  )
     } else {
-      rho_1 <- d_rho_1(rho, parms)
-      rho_11 <- d_rho_11(rho, parms)
       out <- list(c(
-        rho_1 = rho_1,
-        rho_11 = rho_11
+        rho_1 = delta_1,
+        rho_11 = delta_11
       )  )
     }
     return(out)
   },
   meanfield = function(t, rho, parms = model_parms) {
 
-    delta <- d_rho_mean(rho, parms )
+    rho <- ini_rho(rho[1])
+    delta_1 <- (1-rho[1])*colonization(rho, parms) - rho[1]*death(rho, parms)
 
     list(c(
-          rho_1 = delta,
-         rho_11 = delta
+          rho_1 = delta_1,
+         rho_11 = delta_1
          ))
   },
   update = function(x_old, parms, subs = 12) {
