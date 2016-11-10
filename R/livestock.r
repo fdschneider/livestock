406d24629d0465ad6c3855ed0bde1f21a461ae6d
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-11-10 14:34:55 +0100

    fix update function for caspr simulation

--- R/livestock.r
+++ R/livestock.r
@@ -71,9 +71,12 @@ livestock <- list(
     }
     return(out)
   },
+  pairmeanfield = function(t, rho, parms = model_parms) {
+    pair(t, ini_rho(rho[[1]]), parms)
+  },
   meanfield = function(t, rho, parms = model_parms) {
 
-    #rho <- ini_rho(rho[[1]])
+    rho <- ini_rho(rho[[1]])
     delta_1 <- (1-rho[[1]])*colonization(rho, parms) - rho[[1]]*death(rho, parms)
 
     if(rho[[1]]+delta_1 < 1e-6) {
@@ -89,7 +92,7 @@ livestock <- list(
     }
     return(out)
   },
-  update = function(x_old, parms, subs = 12) {  #for cellular automata simulation in package caspr
+  update = function(x_old, parms, subs = 24, i = i) {  #for cellular automata simulation in package caspr
 
     x_new <- x_old
 
@@ -99,23 +102,23 @@ livestock <- list(
 
       # model specific part:
       # 1 - setting time-step parameters
-      rho_one <- sum(x_old$cells == "1")/(x_old$dim[1]*x_old$dim[2]) # get initial vegetation cover
+      rho_one <- as.numeric(sum(x_old$cells == "1")/(x_old$dim[1]*x_old$dim[2])) # get initial vegetation cover
       q_one_one <- neighbors(x_old, "1")/4  # count local density of occupied fields for each cell
 
       # 2 - drawing random numbers
       rnum <- runif(x_old$dim[1]*x_old$dim[2]) # one random number between 0 and 1 for each cell
 
       # 3 - setting transition probabilities
-      growth <- with(parms, colonization(ini_rho(rho_one, q_11 = q_one_one), parms) *1/subs)  # recolonisation rates of all cells
+      growth <- colonization(ini_rho(rho_one, q_11 = q_one_one), parms) *1/subs  # recolonisation rates of all cells
 
       growth[growth < 0] <- 0
 
-      death <- with(parms, death(ini_rho(rho_one, q_11 = q_one_one), parms) *1/subs)   # set probability of death for each cell
+      death <- death(ini_rho(rho_one, q_11 = q_one_one), parms) *1/subs   # set probability of death for each cell
 
       death[death < 0] <- 0
 
       # check for sum of probabilities to be inferior 1 and superior 0
-      if(any(c(growth, death) > 1 )) warning(paste("a set probability is exceeding 1 in time step", i, "! decrease delta!!!"))
+      if(any(c(growth, death) > 1 )) warning(paste("a set probability is exceeding 1 ! decrease delta!!!"))
       #if(any(c(growth, death) < 0)) warning(paste("a set probability falls below 0 in time step", i, "! balance parameters!!!"))
 
       # 4 - apply transition probabilities
